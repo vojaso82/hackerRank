6e4604c8d198c8182b8971adee67de1afc66c01c
vojaso82 <vojislavzaja@gmail.com>
2023-02-06 15:09:32 -0500

    done

---
+++ billDivision/index.js
@@ -0,0 +1,8 @@
+function bonAppetit(bill, k, b) {
+  let result = "";
+  bill.splice(k, 1);
+  const billCost = bill.reduce((previous, next) => previous + next, 0) / 2;
+  if (billCost == b) result = "Bon Appetit";
+  else result = b - billCost;
+  console.log(result);
+}
