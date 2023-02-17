0eb87908868605dcf89ebbb8c3484fc52f3e09a2
vojaso82 <vojislavzaja@gmail.com>
2023-02-17 17:30:58 -0500

    done

---
+++ theHurdleRace/index.js
@@ -0,0 +1,15 @@
+function hurdleRace(k, height) {
+  let maxCount = 0;
+  for (let i = 0; i < height.length; i++) {
+    if (height[i] > k) {
+      let diff = height[i] - k;
+      if (diff > maxCount) {
+        maxCount = diff;
+      }
+    }
+  }
+  if (maxCount === 0) {
+    return 0;
+  }
+  return maxCount;
+}
